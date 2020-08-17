780164e5da49bb140729f2acf031fd8f8a064748
hieuquang2212 <hieuquang2212@gmail.com>
2020-08-17 17:08:12 +0700

    add all files

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store/index';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+       <App />
+  </Provider>,
   document.getElementById('root')
 );
 
